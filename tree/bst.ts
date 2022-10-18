e6a1d96b394a24049ffa282955bd281cf36d7aa2
resqiar <resqiageng@gmail.com>
2022-10-18 22:59:53 +0700

    update bfs

--- tree/bst.ts
+++ tree/bst.ts
@@ -1,3 +1,56 @@
+class QNode {
+  constructor(
+    public bst: BSTNode | null = null,
+    public next: QNode | null = null,
+    public prev: QNode | null = null
+  ) {}
+}
+
+class BSTQueue {
+  constructor(
+    private front: QNode | null = null,
+    private rear: QNode | null = null,
+    public length: number = 0
+  ) {}
+
+  enqueue(value: BSTNode): BSTNode {
+    const newNode = new QNode();
+    newNode.bst = value;
+
+    if (!this.front) {
+      this.front = newNode;
+      this.rear = newNode;
+      this.length++;
+      return newNode.bst;
+    }
+
+    const temp = this.rear;
+    temp!.next = newNode;
+    this.rear = newNode;
+    newNode.prev = temp;
+    this.length++;
+    return newNode.bst;
+  }
+
+  dequeue(): BSTNode | null {
+    if (!this.front) return null;
+
+    const temp = this.front;
+
+    if (this.length === 1) {
+      this.front = null;
+      this.rear = null;
+      this.length--;
+      return temp.bst;
+    }
+
+    this.front = temp.next;
+    this.front!.prev = null;
+    this.length--;
+    return temp.bst;
+  }
+}
+
 class BSTNode {
   constructor(
     public value: number | null = null,
@@ -10,7 +63,7 @@ class BSTNode {
 class BST {
   constructor(public root: BSTNode | null = null) {}
 
-  insert(value: number) {
+  insert(value: number): void {
     const newNode = new BSTNode();
     newNode.value = value;
 
@@ -20,7 +73,8 @@ class BST {
     // If there is no main root, set the new node as
     // a root of the tree.
     if (!ref) {
-      return (this.root = newNode);
+      this.root = newNode;
+      return;
     }
 
     while (ref && ref.value) {
@@ -46,7 +100,8 @@ class BST {
 
       // Increment the freq of the node by 1
       else if (ref.value === value) {
-        return ref.freq++;
+        ref.freq++;
+        return;
       }
 
       // else just break
@@ -54,7 +109,7 @@ class BST {
     }
   }
 
-  traverseLeft() {
+  traverseLeft(): void {
     if (!this.root) return;
 
     let ref = this.root;
@@ -94,23 +149,77 @@ class BST {
 
     return null;
   }
+
+  BFS() {
+    if (!this.root) return;
+    // This array will be the result
+    // after traversing the tree.
+    const result = [];
+    // Create new Queue
+    const q = new BSTQueue();
+
+    // Set initial value of current as this root
+    let current: BSTNode | null = this.root;
+
+    // Push the current root into the queue
+    q.enqueue(current);
+
+    // While there is a value inside the queue, iterate
+    // TAKE A NOTE HERE THAT THE ITERATIONS HERE
+    // HAS A CHANCE FOR DUPLICATES, SO IT WILL NOT EXACTLY
+    // ITERATES THROUGH ALL YOUR INPUT THAT BASED ON RANDOMIZATION.
+    while (q.length) {
+      // set current pointer as the one that has been dequeued
+      current = q.dequeue();
+      // then push the dequeued value to the array
+      result.push(current?.value);
+      // Check if current value (BSTNode) has left,
+      // If it has a left, push that left node into the queue
+      if (current?.left) q.enqueue(current.left);
+      // Check if current value (BSTNode) has right,
+      // If it has a left, push that right node into the queue
+      if (current?.right) q.enqueue(current.right);
+    }
+
+    // finally return the result
+    return result;
+  }
 }
+
+const q = new BSTQueue();
+
+console.log("========= E N Q U E U I N G ============");
+
+for (let i = 0; i < 5; i++) {
+  const node = new BSTNode();
+  node.value = i;
+
+  console.log(q.enqueue(node));
+}
+
+console.log("========= D E Q U E U I N G ============");
+
+for (let i = 0; i < 5; i++) {
+  console.log(q.dequeue());
+}
+
+console.log("========= B S T ============");
 
 const newBst = new BST();
 
-let start = performance.now();
-for (let i = 0; i < 1_000; i++) {
-  newBst.insert(i + 2);
-}
-let end = performance.now();
-console.log("LINEAR INSERT TOOK", (end - start) / 1000, "s");
+// let start = performance.now();
+// for (let i = 0; i < 10_000; i++) {
+//   newBst.insert(i + 2);
+// }
+// let end = performance.now();
+// console.log("LINEAR INSERT TOOK", (end - start) / 1000, "s");
 
-start = performance.now();
-for (let i = 0; i < 1_000; i++) {
-  const rand = Math.floor(Math.random() * 1000);
+let start = performance.now();
+for (let i = 0; i < 100_000; i++) {
+  const rand = Math.floor(Math.random() * 100_000);
   newBst.insert(rand);
 }
-end = performance.now();
+let end = performance.now();
 console.log("BINARY INSERT TOOK", (end - start) / 1000, "s");
 
 start = performance.now();
@@ -119,3 +228,5 @@ end = performance.now();
 
 console.log(res);
 console.log("SEARCH TOOK", end - start, "ms");
+
+console.log(newBst.BFS());
