3e54ef33c500efea628d77554c5af67b0861b85d
resqiar <resqiageng@gmail.com>
2022-11-01 01:44:34 +0700

    add extract max to the binary heap

--- heap/binary-heap.ts
+++ heap/binary-heap.ts
@@ -20,11 +20,83 @@ class MaxBHeap {
     this.value.push(n);
     this.bubble(this.value.length - 1);
   }
+
+  extractMax() {
+    const firstElem = this.value[0];
+    const lastElem = this.value.pop();
+
+    if (!lastElem) return;
+    if (this.value.length !== 0) {
+      this.value[0] = lastElem;
+
+      bh.bubbleDown(0);
+    }
+
+    return firstElem;
+  }
+
+  bubbleDown(parentIdx: number) {
+    const parent = this.value[parentIdx];
+
+    // Left child calculation
+    const childLeftIdx = 2 * parentIdx + 1;
+    const childLeft = this.value[childLeftIdx];
+
+    // Right child calculation
+    const childRightIdx = 2 * parentIdx + 2;
+    const childRight = this.value[childRightIdx];
+
+    // If the there are no childs
+    if (!childLeft && !childRight) return;
+
+    // if the childLeft is bigger than childRight,
+    // while also bigger than parent.
+    if (childLeft > childRight && childLeft > parent) {
+      swapLeft(this);
+    }
+
+    // if the childRight is bigger than childLeft,
+    // while also bigger than parent.
+    else if (childLeft < childRight && childRight > parent) {
+      swapRight(this);
+    }
+
+    // if the childLeft is the same with childRight,
+    // while also bigger than parent.
+    else if (childRight > parent) {
+      swapRight(this);
+    }
+
+    function swapLeft(ctx: MaxBHeap) {
+      ctx.value[childLeftIdx] = parent;
+      ctx.value[parentIdx] = childLeft;
+
+      ctx.bubbleDown(childLeftIdx);
+    }
+
+    function swapRight(ctx: MaxBHeap) {
+      ctx.value[childRightIdx] = parent;
+      ctx.value[parentIdx] = childRight;
+
+      ctx.bubbleDown(childRightIdx);
+    }
+  }
 }
 
 const bh = new MaxBHeap();
 bh.push(20);
 bh.push(1);
-bh.push(5);
 bh.push(6);
+bh.push(6);
+bh.push(2);
+bh.push(7);
+console.log(bh.value);
+console.log(bh.extractMax());
+console.log(bh.extractMax());
+console.log(bh.extractMax());
+console.log(bh.extractMax());
+console.log(bh.extractMax());
+console.log(bh.extractMax());
+console.log(bh.extractMax());
+console.log(bh.extractMax());
 console.log(bh.value);
