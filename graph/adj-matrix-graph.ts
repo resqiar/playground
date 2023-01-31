70740937f6df8867953a4b021c38f2c07868c7f7
resqiar <resqiageng@gmail.com>
2023-02-01 05:56:43 +0700

    update

--- graph/adj-matrix-graph.ts
+++ graph/adj-matrix-graph.ts
@@ -1,4 +1,90 @@
 namespace matrix {
+  class PriorityQueue {
+    constructor(public heap: { vtx: string; priority: number }[] = []) {}
+
+    enqueue(vtx: string, priority: number) {
+      this.heap.push({ vtx, priority });
+
+      // Bubble Up
+      this.bubbleUp(this.heap.length - 1);
+    }
+
+    dequeue() {
+      // save the first and last element of the array
+      const dequeued = this.heap[0];
+      const lastElem = this.heap.pop();
+
+      if (!lastElem) return;
+
+      // swap the last index of the heap array to
+      // the first index
+      if (this.heap.length !== 0) {
+        this.heap[0] = lastElem;
+
+        // bubble down the first index
+        this.bubbleDown(0);
+      }
+
+      return dequeued;
+    }
+
+    bubbleUp(idx: number) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+
+      const parent = this.heap[parentIdx];
+      const current = this.heap[idx];
+
+      if (!parent) return;
+
+      if (current.priority < parent.priority) {
+        this.heap[parentIdx] = current;
+        this.heap[idx] = parent;
+        this.bubbleUp(parentIdx);
+      }
+    }
+
+    bubbleDown(idx: number) {
+      const parent = this.heap[idx];
+
+      const leftChildIdx = 2 * idx + 1;
+      const leftChild = this.heap[leftChildIdx];
+
+      const rightChildIdx = 2 * idx + 2;
+      const rightChild = this.heap[rightChildIdx];
+
+      // if there is no children of the current parent
+      if (!leftChild && !rightChild) return;
+
+      if (
+        rightChild &&
+        leftChild.priority < parent.priority &&
+        leftChild.priority < rightChild.priority
+      ) {
+        swapLeft(this);
+      } else if (
+        rightChild &&
+        rightChild.priority < parent.priority &&
+        rightChild.priority < leftChild.priority
+      ) {
+        swapRight(this);
+      } else if (leftChild.priority < parent.priority) {
+        swapLeft(this);
+      }
+
+      function swapLeft(ctx: PriorityQueue) {
+        ctx.heap[idx] = leftChild;
+        ctx.heap[leftChildIdx] = parent;
+        ctx.bubbleDown(leftChildIdx);
+      }
+
+      function swapRight(ctx: PriorityQueue) {
+        ctx.heap[idx] = rightChild;
+        ctx.heap[rightChildIdx] = parent;
+        ctx.bubbleDown(rightChildIdx);
+      }
+    }
+  }
+
   interface Edge {
     weight: number;
   }
@@ -54,4 +140,17 @@ namespace matrix {
 
   console.log("======= I N D I C E S =======");
   console.log(g.indices);
+
+  const pq = new PriorityQueue();
+  console.log(pq.enqueue("SBY", 5));
+  console.log(pq.enqueue("JKT", 2));
+  console.log(pq.enqueue("BKS", 3));
+  console.log(pq.enqueue("MLG", 7));
+  console.log(pq.enqueue("BWN", 1));
+
+  console.log(pq.heap);
+
+  while (pq.heap.length) {
+    console.log(pq.dequeue());
+  }
 }
