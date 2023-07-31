7e14eeaf510b48dfa6dde7d17125c1304143ccd7
resqiar <resqiageng@gmail.com>
2023-08-01 01:10:34 +0700

    update

--- go/channels/test_channel.go
+++ go/channels/test_channel.go
@@ -2,6 +2,7 @@ package channels
 
 import (
 	"log"
+	"sync"
 )
 
 func TestChannel() {
@@ -25,7 +26,63 @@ func TestChannel() {
 		}
 	}()
 
+	// a technique called ranging,
+	// range in for loop can accept channel.
+	// automatically break when channel is closed.
 	for result := range dataStream {
 		log.Println(result)
+	}
+
+	beginStream := make(chan int)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			defer wg.Done()
+			val := <-beginStream
+			log.Println(i, val)
+		}(i)
+	}
+
+	log.Println("BEFORE CLOSING CHANNEL")
+
+	// when you close the stream, a channel can read
+	// the channel indefinitely without no-one sending anything.
+	close(beginStream)
+	wg.Wait()
+
+	// buffered channel.
+	// when channel created with specified capacity,
+	// it is called a buffered channel. The channel then can perform-
+	// N (capacity) times of write even though no one is pulling/reading the data.
+	bufferedStream := make(chan int, 5)
+
+	for i := 0; i < 6; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			defer wg.Done()
+			bufferedStream <- i
+		}(i)
+	}
+
+	log.Println("PULL & READ ONCE:", <-bufferedStream)
+	wg.Wait()
+
+	const STREAM_CON int = 5
+	intStream := make(chan int, STREAM_CON-1)
+	go func() {
+		defer close(intStream)
+		defer log.Println("Producer Done")
+
+		for i := 0; i < STREAM_CON; i++ {
+			log.Println("Sending:", i)
+			intStream <- i
+		}
+	}()
+
+	for result := range intStream {
+		log.Println("Received:", result)
 	}
 }
